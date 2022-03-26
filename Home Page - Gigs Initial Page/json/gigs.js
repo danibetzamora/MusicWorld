5d4c243151a3953eadee62429e7484b9766b25f0
danibetzamora <danielbetancor2000@gmail.com>
2022-03-26 14:29:22 +0000

    Added dynamic load from a json file to AllGigs page

---
+++ Home Page - Gigs Initial Page/json/gigs.js
@@ -0,0 +1,50 @@
+var gigs = [
+    {
+        "image" : "images/gig1.png",
+        "profileImage" : "images/gig1-profile.png",
+        "profileName" : "remonbeeftink",
+        "description" : "I will write and produce you amazing music for all your projects",
+        "ratingImage" : "images/star.png",
+        "rating" : "4.9",
+        "ratingsNumber" : "85",
+        "optionIcon" : "images/options-icon.png",
+        "heartIcon" : "images/heart-icon.png",
+        "price" : "€32"
+    },
+    {
+        "image" : "images/gig2.png",
+        "profileImage" : "images/gig2-profile.png",
+        "profileName" : "aslan",
+        "description" : "I will be your music producer composer for your music production",
+        "ratingImage" : "images/star.png",
+        "rating" : "4.6",
+        "ratingsNumber" : "228",
+        "optionIcon" : "images/options-icon.png",
+        "heartIcon" : "images/heart-icon.png",
+        "price" : "€15"
+    },
+    {
+        "image" : "images/gig3.png",
+        "profileImage" : "images/gig3-profile.png",
+        "profileName" : "jackestone",
+        "description" : "I will compose the perfect music for your video game",
+        "ratingImage" : "images/star.png",
+        "rating" : "4.8",
+        "ratingsNumber" : "507",
+        "optionIcon" : "images/options-icon.png",
+        "heartIcon" : "images/heart-icon.png",
+        "price" : "€46"
+    },
+    {
+        "image" : "images/gig4.png",
+        "profileImage" : "images/gig4-profile.png",
+        "profileName" : "liljames",
+        "description" : "I will produce custom lofi hip hop chill music beats",
+        "ratingImage" : "images/star.png",
+        "rating" : "4.5",
+        "ratingsNumber" : "932",
+        "optionIcon" : "images/options-icon.png",
+        "heartIcon" : "images/heart-icon.png",
+        "price" : "€27"
+    }
+]
