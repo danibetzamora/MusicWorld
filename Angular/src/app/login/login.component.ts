b4ad7412d1f52a2677bda85f568ef7f2b5d7e37f
alelele5946 <av5946@gmail.com>
2022-04-20 12:45:16 +0100

    login validation fixed

--- Angular/src/app/login/login.component.ts
+++ Angular/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild , AfterViewInit, ElementRef} from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+loginForm = this.formBuilder.group({
+  email: "", password:""
+});
 
-  constructor() { }
+
+submitLoginForm(): void{
+  let form = this.loginForm.value;
+  console.log(form);
+}
+
+  constructor(private formBuilder : FormBuilder, private elementRef : ElementRef) { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(){
+
+    var button = this.elementRef.nativeElement.querySelector("#loginButton");
+            button.addEventListener("click", this.sendit);
+            var form = this.elementRef.nativeElement.querySelector("form[name='login']");
+            form.addEventListener("invalid", this.validation, true);
+            form.addEventListener("input", this.checkval);
+        
+  }
+
+  sendit(){
+    let form = $("form[name='login']");
+    var valid = (<HTMLInputElement>document.querySelector("form[name='login']")).checkValidity();
+
+    if(valid) {
+        alert('¡Login successfully!');
+        form.submit();
+    } else {
+        alert('You must fill in all fields and take into account the format of each one. (If you hover the mouse pointer over each one you can get more information about it).');
+    } 
+
+    
+  }  
+  validation(e: any){
+    var elem = e.target;
+    elem.style.background = '#FFDDDD';
 }
+checkval(e: any){
+  var elem = e.target;
+
+  if(elem.validity.valid){
+      elem.style.background = '#96FF8D';
+  }else{
+      elem.style.background = '#FFDDDD';
+  }
+}
+
+
+
+}
+
+
+
