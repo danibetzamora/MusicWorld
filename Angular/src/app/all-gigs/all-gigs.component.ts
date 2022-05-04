fb90c289d72eda49d5f611c76a5b876bb5132336
danibetzamora <danielbetancor2000@gmail.com>
2022-05-04 12:38:59 +0100

    Load allGigs from firebase

--- Angular/src/app/all-gigs/all-gigs.component.ts
+++ Angular/src/app/all-gigs/all-gigs.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Gig } from "../gig.model";
+import { ActivatedRoute, ParamMap } from '@angular/router'
+import { GigService} from "../gig.service";
 
 @Component({
   selector: 'app-all-gigs',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllGigsComponent implements OnInit {
 
-  constructor() { }
+  gigs: Gig[] = [];
+
+  constructor(
+    private gigService: GigService,
+  ) { }
 
   ngOnInit(): void {
+    this.getGigs();
   }
 
+  public getGigs(){
+    this.gigService.getList().subscribe(
+      (res: any) => this.gigs = res.map(
+        (item: any) => ({ ...item.data(), 'id': item.id})
+      ) as Gig[]
+    );
+  }
+/*
+  public getGigs() {
+    this.gigService.getList().subscribe(
+      (res: any) => this.gigs = res.map(
+        (item: any) => ({ ...item.data(), 'id': item.id})
+      ).filter(
+        (item: any) => item.clicks == undefined
+      )as Gig[]
+    );
+  }
+*/
 }
